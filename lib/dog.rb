8711258f0fbf6aa2ec4c1938247ad7501194f8f4
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-04-01 22:25:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
+c
+
 class Dog
   attr_reader :name
   @@all_dogs = [ ]
   def initialize(name)
     @name = name
-    @@all_dogs << self.name 
+    @@all_dogs << self.name
   end
 
   def self.all
